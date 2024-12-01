a05f33de59549d24f3abb559d366b2b2fdd8bf9a
TaikiKunimoto <taiki416avicii@gmail.com>
2024-12-01 19:06:30 +0900

    シミュレーション環境の設定を追加し、車両の流入時間をランダムに決定する機能を実装。シミュレーションの継続条件を時間と車両数で確認するように修正。コマンドライン引数からの設定を反映。

--- TraCI/main.py
+++ TraCI/main.py
@@ -1,44 +1,117 @@
 import optparse
+import os
+import random
+import sys
+from datetime import datetime
 
 import traci
+from func.cav import CAV
 from sumolib import checkBinary
 
+simulation_time = 60.0
 
-def run():
-    while shouldContinueSim():
+vehicle_instance = []
+veh_id = 0
+alpha = 0.0
+departTime_lane0 = []
+departTime_lane1 = []
+
+
+def run(alpha=0.0, inflow_0=750, inflow_1=750):
+    set_environment(inflow_0, inflow_1)
+
+    while shouldContinueSimWithSimulationTime():
         traci.simulationStep()
+        add_vehicle(alpha)
+
     traci.close()
 
 
+# シミュレーションを開始する
 def startSim():
-    """Starts the simulation."""
-    traci.start(
-        [
-            sumoBinary,
-            "--net-file",
-            "../config/high-way.net.xml",
-            "--route-files",
-            "../config/high-way.rou.xml",
-            "--delay",
-            "200",
-            "--gui-settings-file",
-            "../config/high-way.settings.xml",
-            "--start",
-        ]
-    )
+    traci.start([sumoBinary, "-c", "../config/high-way.sumocfg"])
+    print("Simulation started")
 
 
-def shouldContinueSim():
-    """Checks that the simulation should continue running.
-    Returns:
-        bool: `True` if there are any vehicles on or waiting to enter the network. `False` otherwise.
-    """
+# 初期設定（車両の流入時間の設定）
+def set_environment(inflow_0: int, inflow_1: int):
+    global vehicle_instance
+    global veh_id
+    global departTime_lane0, departTime_lane1
+
+    k_0 = int((simulation_time / 3600) * inflow_0)
+    k_1 = int((simulation_time / 3600) * inflow_1)
+
+    # 車両の流入時刻を決定
+    departTime_lane0 = sorted(random.sample(range(int(simulation_time / 2.0)), k_0))
+    departTime_lane1 = sorted(random.sample(range(int(simulation_time / 2.0)), k_1))
+
+    # 2秒以上開けて流入
+    departTime_lane0 = [round((n * 2.0), 1) + 0.1 for n in departTime_lane0]
+    departTime_lane1 = [round((n * 2.0), 1) + 0.1 for n in departTime_lane1]
+
+    print(departTime_lane0)
+    print(departTime_lane1)
+
+
+def shouldContinueSimWithVehiclesCount():
     numVehicles = traci.simulation.getMinExpectedNumber()
     return True if numVehicles > 0 else False
 
 
+def shouldContinueSimWithSimulationTime():
+    sumo_time = traci.simulation.getTime()
+    if sumo_time % 1 == 0:
+        now = datetime.now().time()
+        print(
+            "====================================================",
+            "\nTIME:",
+            sumo_time,
+            "\nNow:",
+            now,
+            # file=sys.stderr,
+        )
+    return True if sumo_time != simulation_time else False
+
+
+def add_vehicle(alpha):
+    global veh_id
+    global departTime_lane0, departTime_lane1
+    sumo_time = traci.simulation.getTime()
+
+    alpha = float(alpha)
+
+    # if sumo_time in departTime_lane0:
+    if sumo_time in departTime_lane0:
+        traci.vehicle.add(
+            vehID=str(veh_id),
+            routeID="r_pass",
+            typeID="CAV_0",
+            departLane="best",
+            departPos="base",
+            departSpeed="16.67",
+        )
+        instance = CAV(veh_id, alpha, withAgree=True)
+        vehicle_instance.append(instance)
+        veh_id += 1
+
+    # if sumo_time in departTime_lane1:
+    if sumo_time in departTime_lane1:
+        traci.vehicle.add(
+            vehID=str(veh_id),
+            routeID="r_exit",
+            typeID="CAV_1",
+            departLane="best",
+            departPos="base",
+            departSpeed="16.67",
+        )
+        instance = CAV(veh_id, alpha, withAgree=True)
+        vehicle_instance.append(instance)
+        veh_id += 1
+
+
 def get_options():
-    """define options for this script and interpret the command line"""
+    # define options for this script and interpret the command line
     optParser = optparse.OptionParser()
     optParser.add_option(
         "--nogui",
@@ -51,7 +124,14 @@ def get_options():
 
 
 if __name__ == "__main__":
+    # コマンドライン引数を取得
     options = get_options()
+    args = sys.argv
+    alpha = str(args[1])  #
+    seed = args[2]  # 乱数のシード(等しいseedで実行すると同じ結果が得られる)
+    random.seed(seed)
+    inflow_0 = int(args[3])  # 車両の流入数
+    inflow_1 = int(args[4])  # 車両の流入数
 
     # this script has been called from the command line. It will start sumo as a server, then connect and run
     if options.nogui:
@@ -60,4 +140,4 @@ if __name__ == "__main__":
         sumoBinary = checkBinary("sumo-gui")
 
     startSim()
-    run()
+    run(alpha, inflow_0, inflow_1)
